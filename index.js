f1ee90466d749eab3a830ec12c33a15ecb52d672
Edwin Lin <edwinlin@gmail.com>
2018-11-13 03:16:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const app = "I don't do much."
 var dodger = document.getElementById('dodger')
 function moveDodgerRight() {
-  var rightNumbers = dodger.style.right.replace('px', '')
+  var rightNumbers = dodger.style.right
   var right = parseInt(rightNumbers, 10)
  
-  if (right > 0) {
-    dodger.style.right = `${right + 1}px`
-  }
+ if (right > 0 ){
+  dodger.style.right = `${right - 1}px`
+ }
 }
 
 document.addEventListener('keydown', function(e) {
